968b6347c77781fef0d112b0e37cdd3826bdd855
sungwoo.yoon <sungwoo.yoon@lge.com>
2020-06-10 16:45:35 +0900

    Add RxPlugin ErrorHandler for UndeliverableException
    [DESC] UndeliverableException 발생 시, 앱 죽는 이슈 수정

--- app/src/main/java/com/promise09th/mvvmproject/MvvmApplication.java
+++ app/src/main/java/com/promise09th/mvvmproject/MvvmApplication.java
@@ -2,14 +2,20 @@ package com.promise09th.mvvmproject;
 
 import android.app.Activity;
 import android.app.Application;
+import android.util.Log;
 
 import com.promise09th.mvvmproject.presentation.di.component.DaggerAppComponent;
+
+import java.io.IOException;
+import java.net.SocketException;
 
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
+import io.reactivex.exceptions.UndeliverableException;
+import io.reactivex.plugins.RxJavaPlugins;
 
 public class MvvmApplication extends Application implements HasActivityInjector {
 
@@ -29,5 +35,36 @@ public class MvvmApplication extends Application implements HasActivityInjector 
                 .application(this)
                 .build()
                 .inject(this);
+
+        RxJavaPlugins.setErrorHandler(e -> {
+            if (e instanceof UndeliverableException) {
+                e = e.getCause();
+            }
+
+            if ((e instanceof IOException) || (e instanceof SocketException)) {
+                // fine, irrelevant network problem or API that throws on cancellation
+                return;
+            }
+
+            if (e instanceof InterruptedException) {
+                // fine, some blocking code was interrupted by a dispose call
+                return;
+            }
+
+            if ((e instanceof NullPointerException) || (e instanceof IllegalArgumentException)) {
+                // that's likely a bug in the application
+                Thread.currentThread().getUncaughtExceptionHandler()
+                        .uncaughtException(Thread.currentThread(), e);
+                return;
+            }
+
+            if (e instanceof IllegalStateException) {
+                // that's a bug in RxJava or in a custom operator
+                Thread.currentThread().getUncaughtExceptionHandler()
+                        .uncaughtException(Thread.currentThread(), e);
+                return;
+            }
+            Log.e("RxJava_HOOK", "Undeliverable exception received, not sure what to do" + e.getMessage());
+        });
     }
 }
